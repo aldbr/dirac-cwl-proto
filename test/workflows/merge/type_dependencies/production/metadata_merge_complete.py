ac97eef5ca3d1584743a2d4b3978fe516d513aa6
ARRABITO Luisa <arrabito@in2p3.fr>
2025-01-15 17:43:44 +0100

    add test

---
+++ test/workflows/merge/type_dependencies/production/metadata_merge_complete.py
@@ -0,0 +1,11 @@
+simulate_step1:
+  metadata:
+    type: PiSimulate_v2
+simulate_step2:
+  metadata:
+    type: PiSimulate_v2
+gathering:
+  metadata:
+    type: PiGather
+    group_size:
+      input-data: 2
