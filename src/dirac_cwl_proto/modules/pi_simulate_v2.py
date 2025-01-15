0fe4e249c74ed81840a81ba9d189d65647dcdfa0
ARRABITO Luisa <arrabito@in2p3.fr>
2025-01-15 14:05:16 +0100

    add merge workflow

---
+++ src/dirac_cwl_proto/modules/pi_simulate_v2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import random
+
+import typer
+from rich.console import Console
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def simulate(
+    num_points: int = typer.Argument(..., help="Number of random points to generate"),
+    output_path: str = typer.Argument(
+        None, help="Output file"),
+):
+    """
+    Simulate random points inside a square (Monte Carlo method).
+    """
+    points = []
+
+    for _ in range(num_points):
+        x, y = random.uniform(-1, 1), random.uniform(-1, 1)
+        points.append((x, y))
+
+    # Save points to file
+
+    with open(output_path, "w") as f:
+        for point in points:
+            f.write(f"{point[0]} {point[1]}\n")
+
+    console.print(f"Generated [bold green]{num_points}[/bold green] random points.")
+    return output_path
+
+
+if __name__ == "__main__":
+    app()
