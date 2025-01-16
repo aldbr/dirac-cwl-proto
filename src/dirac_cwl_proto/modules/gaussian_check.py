b2b1513198b3796901a5bf01f32256be4db18238
natthan-pigoux <natthan.pigoux@gmail.com>
2025-01-16 10:45:13 +0100

    fix script to take several input data files

--- src/dirac_cwl_proto/modules/gaussian_check.py
+++ src/dirac_cwl_proto/modules/gaussian_check.py
@@ -1,27 +1,26 @@
-import time
-
 import typer
 from rich.console import Console
 
 app = typer.Typer()
 console = Console()
 
-@app.command()
-def benchmark(input_data: str, output_data: str):
-    start_time = time.time()
-    
+output: str = typer.Option("fit.txt", "--output", "-o", help="Output file")
+
+
+def gaussian_check(input_data: str):
     with open(input_data, 'r') as f:
         data = f.readlines()
-        # check that the data are represented by a gaussian distribution
         mu = sum(map(float, data)) / len(data)
-        sig = sum([(float(x) - mu) ** 2 for x in data]) / len(data)
-        console.print(f"Mean: {mu}, Standard deviation: {sig}")
+        sigma = sum([(float(x) - mu) ** 2 for x in data]) / len(data)
+        console.print(f"Mean: {mu}, Std dev: {sigma}")
+        return mu, sigma
     
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    console.print(f"Elapsed time: {elapsed_time} seconds")
-    with open(output_data, 'w') as f:
-        f.write(f"Mean: {mu}, Standard deviation: {sig}, Time: {elapsed_time}\n")
+@app.command()
+def main(input_data_files: list[str], output_data = output):
+    for input_data in input_data_files:
+        mu, sigma = gaussian_check(input_data)
+        with open(output_data, 'a') as f:
+            f.write(f"{input_data}: Mean: {mu}, Std dev: {sigma}\n")
 
 if __name__ == "__main__":
     app()
