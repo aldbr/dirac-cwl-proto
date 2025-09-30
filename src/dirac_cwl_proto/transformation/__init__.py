29520a11914dd823016c4f2630b5be36524fc4ba
Loxeris <30194187+Loxeris@users.noreply.github.com>
2025-09-30 17:28:06 +0200

    feat: add lfns_input to job input model

--- src/dirac_cwl_proto/transformation/__init__.py
+++ src/dirac_cwl_proto/transformation/__init__.py
@@ -226,7 +226,7 @@ def _get_inputs(input_query: Path | list[Path], group_size: int) -> List[List[st
 
 
 def _generate_job_model_parameter(
-    input_data_dict: Dict[str, List[List[str]]]
+    input_data_dict: Dict[str, List[List[str]]],
 ) -> List[JobInputModel]:
     """Generate job model parameters from input data provided."""
     job_model_params = []
@@ -238,11 +238,16 @@ def _generate_job_model_parameter(
     ]
     for group in grouped_input_data:
         cwl_inputs = {}
+        lfns: dict[str, Path] = {}
         for input_name, input_data in group.items():
             cwl_inputs[input_name] = [
                 File(path="lfn:" + str(Path(path).resolve())) for path in input_data
             ]
+            for path in input_data:
+                lfns[input_name] = Path(path="lfn:" + str(Path(path).resolve()))
 
-        job_model_params.append(JobInputModel(sandbox=None, cwl=cwl_inputs))
+        job_model_params.append(
+            JobInputModel(sandbox=None, cwl=cwl_inputs, lfns_input=lfns)
+        )
 
     return job_model_params
