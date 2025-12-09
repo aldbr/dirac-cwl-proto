c28a5525b33f4fd31275ccbc2d21caf56d5565e0
Ryunosuke O'Neil <r.oneil@cern.ch>
2025-12-09 18:13:26 +0100

    feat: replica catalog generator using DIRAC

--- src/dirac_cwl_proto/job/replica_catalog.py
+++ src/dirac_cwl_proto/job/replica_catalog.py
@@ -1,3 +1,5 @@
+import enum
+import logging
 import re
 from typing import Annotated
 
@@ -9,6 +11,11 @@ from pydantic import (
     RootModel,
     field_validator,
 )
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+from DIRAC.Core.Utilities.ReturnValues import returnValueOrRaise
 
 
 def validateLFN(value: str) -> str:
@@ -101,7 +108,7 @@ class ReplicaCatalog(RootModel):
                 None  # Like 6032CB7C-32DC-EC11-9A66-D85ED3091D71
             )
 
-            @field_validator("adler32", "md5", "guid")
+            @field_validator("adler32", "guid")
             @classmethod
             def validate_at_least_one_checksum(cls, v, info):
                 # This validator runs for each field, so we check after all fields are processed
@@ -132,3 +139,116 @@ class ReplicaCatalog(RootModel):
 
     def __getitem__(self, item):
         return self.root[item]
+
+
+class InputDataResolution(enum.StrEnum):
+    Protocol = "InputDataByProtocol"
+    Download = "DownloadInputData"
+
+
+def dirac_make_replica_catalog(
+    input_lfns, local_site_name=None, resolver=InputDataResolution.Protocol
+):
+    if resolver == InputDataResolution.Protocol:
+        from DIRAC.WorkloadManagementSystem.Client.InputDataByProtocol import (
+            InputDataByProtocol,
+        )
+
+        logger.info("ℹ️  Using InputDataByProtocol for input data resolution")
+        module = InputDataByProtocol
+    elif resolver == InputDataResolution.Download:
+        from DIRAC.WorkloadManagementSystem.Client.DownloadInputData import (
+            DownloadInputData,
+        )
+
+        module = DownloadInputData
+        logger.info("ℹ️  Using DownloadInputData for input data resolution")
+    else:
+        raise ValueError(f"Unknown input data resolution method: {resolver}")
+
+    from DIRAC import gConfig
+    from DIRAC import siteName as DIRAC_siteName
+    from DIRAC.Core.Utilities.SiteSEMapping import getSEsForSite
+    from DIRAC.Interfaces.API.Dirac import Dirac as Dirac_Api
+
+    api = Dirac_Api()
+
+    diskSE = gConfig.getValue(
+        api.section + "/DiskSE", ["-disk", "-DST", "-USER", "-FREEZER"]
+    )
+    tapeSE = gConfig.getValue(api.section + "/TapeSE", ["-tape", "-RDST", "-RAW"])
+
+    if not local_site_name:
+        siteName = DIRAC_siteName()
+    else:
+        siteName = local_site_name
+
+    # fetch local SEs for the site
+    localSEList = returnValueOrRaise(getSEsForSite(siteName))
+    logger.info(f"Local SEs for site {siteName}: {localSEList}")
+
+    # Get replicas for jobs from the LFNs
+    logger.info(f"Attempting to resolve data for {siteName}")
+    replicaDict = returnValueOrRaise(api.getReplicasForJobs(input_lfns))
+    if replicaDict.get("Failed", {}):
+        raise ValueError(
+            f"Failed to resolve replicas for LFNs: {replicaDict['Failed']}"
+        )
+    replicaDict = replicaDict["Successful"]
+
+    # get file metadata from the LFNs
+    guidDict = returnValueOrRaise(api.getLfnMetadata(input_lfns))
+    if guidDict.get("Failed", {}):
+        raise ValueError(f"Failed to get metadata for LFNs: {guidDict['Failed']}")
+    guidDict = guidDict["Successful"]
+
+    # prepare arguments for the module
+
+    # for the Input Data Resolution module
+    # merge replica info into guidDict
+    for lfn, reps in replicaDict.items():
+        guidDict[lfn].update(reps)
+    resolvedData = guidDict.copy()
+
+    argumentsDict = {
+        "FileCatalog": {"Value": {"Successful": resolvedData}},
+        "Configuration": {
+            "JobID": None,
+            "LocalSEList": localSEList,
+            "DiskSEList": diskSE,
+            "TapeSEList": tapeSE,
+            "SiteName": siteName,
+        },
+        "InputData": input_lfns,
+    }
+
+    logger.debug(argumentsDict)
+
+    # execute the module
+    result = returnValueOrRaise(module(argumentsDict).execute())
+    logger.debug(result)
+
+    # Start building the ReplicaCatalog
+    catalog = ReplicaCatalog.model_validate(
+        {
+            lfn: ReplicaCatalog.CatalogEntry(
+                replicas=[
+                    ReplicaCatalog.CatalogEntry.Replica(
+                        url=replica_url,
+                        se=replica_se,
+                    )
+                    for replica_se, replica_url in replicaDict[lfn].items()
+                ],
+                size_bytes=file_info.get("Size", None),
+                checksum=ReplicaCatalog.CatalogEntry.Checksum(
+                    guid=file_info.get("GUID", None),
+                    adler32=file_info["Checksum"]
+                    if file_info["ChecksumType"] == "Adler32"
+                    else None,
+                ),
+            )
+            for lfn, file_info in guidDict.items()
+        }
+    )
+
+    return catalog
